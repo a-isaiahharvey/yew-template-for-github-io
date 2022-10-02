5dc5cbefb563c8507546de365a3ec20d21004ca3
Allister Isaiah Harvey <41553768+a-isaiahharvey@users.noreply.github.com>
2022-10-02 08:23:34 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,15 @@
-const fs = require('fs');
-
-function findAllRsExtensions(dir) {
-  const extensions = [];
-  const files = fs.readdirSync(dir);
-  files.forEach(file => {
-    const filePath = `${dir}/${file}`;
-    const stat = fs.statSync(filePath);
-    if (stat.isDirectory()) {
-      extensions.push(...findAllRsExtensions(filePath));
-    } else if (file.endsWith('.rs')) {
-      extensions.push(filePath);
-    }
-  });
-  return extensions;
-}
-
 module.exports = {
-  content: findAllRsExtensions("./src"),
+  content: content: [
+        "./src/**/*.rs",
+        "./index.html",
+        "./src/**/*.html",
+        "./src/**/*.css",
+    ],
   theme: {
     container: {
       center: true,
     }
   },
+  variants: {},
   plugins: [],
 }
